e38cf24c85239852e9873cbc8b41ea25fb861ead
Roza Ghamari <rozagh@rozagh-macbookpro.roam.corp.google.com>
2019-01-15 17:03:24 -0800

    fix yaml

--- app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.myfirstapp", appContext.getPackageName());
+        
     }
 }
