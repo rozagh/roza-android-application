19d4816c66447751b26aaa8270ba446937645ab5
Roza Ghamari <roza.gh@gmail.com>
2019-01-28 17:34:11 -0800

    changes to add fastlane

--- app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
@@ -2,10 +2,17 @@ package com.example.myfirstapp;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 import static org.junit.Assert.*;
 
@@ -16,6 +23,9 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -23,5 +33,15 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.roza.myfirstapp", appContext.getPackageName());
 
+    }
+
+    @Test
+    public void testTakeScreenshot() {
+        Screengrab.screenshot("before_button_click");
+
+        // Your custom onView...
+        Espresso.onView(ViewMatchers.withId(R.id.button)).perform(ViewActions.click());
+
+        Screengrab.screenshot("after_button_click");
     }
 }
